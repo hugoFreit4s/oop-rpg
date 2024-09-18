c2ec031df1c90c9caf9dfc833492af1fafae644b
Hugo de Freitas <hugo.encaminhados@gmail.com>
2024-09-18 12:39:15 -0300

    Creating function to handle color changes on the life bar

--- index.js
+++ index.js
@@ -5,32 +5,15 @@ const atkBtn = document.getElementById('atk_btn');
 const pLifeBar = document.getElementById('pcurrent_life_bar');
 const mLifeBar = document.getElementById('mcurrent_life_bar');
 const p = new Player(50, 50, 3, 1, 'Eu');
-const mob = new Monster(10, 10, 5, 1, 'Rat', 'Human');
+const m = new Monster(10, 10, 5, 1, 'Rat', 'Human');
 
 atkBtn.addEventListener('click', () => {
-    turno(p, mob);
-});
-
-function turno(p, m) {
-    let playerBarColor;
-    let monsterBarColor;
-
     p.setExp(1);
 
     const damageOnMonster = p.atk - m.def;
     m.hp -= damageOnMonster;
     const mLifeBarPercentage = (m.hp / m.maxHp) * 100;
     mLifeBar.style.width = `${mLifeBarPercentage.toString()}%`;
-
-    if (mLifeBarPercentage > 60) {
-        monsterBarColor = "#00FF00";
-    } else if (mLifeBarPercentage > 30) {
-        monsterBarColor = "#A3FF00";
-    } else {
-        monsterBarColor = "#FFFF00";
-    }
-
-    mLifeBar.style.backgroundColor = monsterBarColor;
 
     console.log(`Monstro sofreu um ataque de ${damageOnMonster} de dano!`);
     console.log(`Monster HP: ${m.hp}/${m.maxHp}`);
@@ -40,15 +23,7 @@ function turno(p, m) {
     const pLifeBarPercentage = (p.hp / p.maxHp) * 100;
     pLifeBar.style.width = `${pLifeBarPercentage.toString()}%`;
 
-    if (pLifeBarPercentage > 60) {
-        playerBarColor = "#00FF00";
-    } else if (pLifeBarPercentage > 30) {
-        playerBarColor = "#A3FF00";
-    } else {
-        playerBarColor = "#FFFF00";
-    }
-
-    pLifeBar.style.backgroundColor = playerBarColor;
+    setColorBar(pLifeBarPercentage, mLifeBarPercentage);
 
     console.log(`Player sofreu um ataque de ${damageOnPlayer} de dano!`);
     console.log(`Player HP: ${p.hp}/${p.maxHp}`);
@@ -64,6 +39,32 @@ function turno(p, m) {
 
     if (m.hp <= 0) {
         console.log('Monstro derrotado!');
+        mLifeBar.style.width = '100%';
         m.respawnMonster();
+        setColorBar(pLifeBarPercentage, 100);
     }
+});
+
+function setColorBar(pPercentage, mPercentage) {
+    let playerBarColor;
+    let monsterBarColor;
+
+    if (pPercentage > 60) {
+        playerBarColor = '#00FF00';
+    } else if (pPercentage > 30) {
+        playerBarColor = '#A3FF00';
+    } else {
+        playerBarColor = '#FFFF00';
+    }
+
+    if (mPercentage > 60) {
+        monsterBarColor = "#00FF00";
+    } else if (mPercentage > 30) {
+        monsterBarColor = '#A3FF00';
+    } else {
+        monsterBarColor = '#FFFF00';
+    }
+
+    pLifeBar.style.backgroundColor = playerBarColor;
+    mLifeBar.style.backgroundColor = monsterBarColor;
 }
